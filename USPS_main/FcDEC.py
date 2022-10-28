a7c1db9541922bae40ba32257d7291b318889689
TriMates <74346251+TriMates@users.noreply.github.com>
2022-10-28 20:54:41 +0800

    Update FcDEC.py

--- USPS_main/FcDEC.py
+++ USPS_main/FcDEC.py
@@ -1,11 +1,4 @@
-"""
-Tensorflow implementation for FcDEC and FcDEC-DA algorithms:
-    - Junyuan Xie, Ross Girshick, and Ali Farhadi. Unsupervised deep embedding for clustering analysis. ICML 2016.
-    - Xifeng Guo, En Zhu, Xinwang Liu, and Jianping Yin. Deep Embedded Clustering with Data Augmentation. ACML 2018.
 
-Author:
-    Xifeng Guo. 2018.6.30
-"""
 
 from time import time
 import numpy as np
@@ -22,58 +15,31 @@ import metrics
 
 
 def autoencoder(dims, act='relu'):
-    """
-    Fully connected auto-encoder model, symmetric.
-    Arguments:
-        dims: list of number of units in each layer of encoder. dims[0] is input dim, dims[-1] is units in hidden layer.
-            The decoder is symmetric with encoder. So number of layers of the auto-encoder is 2*len(dims)-1
-        act: activation, not applied to Input, Hidden and Output layers
-    return:
-        (ae_model, encoder_model), Model of autoencoder and model of encoder
-    """
+
     n_stacks = len(dims) - 1
     init = VarianceScaling(scale=1. / 3., mode='fan_in', distribution='uniform')
 
-    # input
+
     x = Input(shape=(dims[0],), name='input')
     h = x
 
-    # internal layers in encoder
     for i in range(n_stacks-1):
         h = Dense(dims[i + 1], activation=act, kernel_initializer=init, name='encoder_%d' % i)(h)
 
-    # hidden layer
-    h = Dense(dims[-1], kernel_initializer=init, name='encoder_%d' % (n_stacks - 1))(h)  # hidden layer, features are extracted from here
 
+    h = Dense(dims[-1], kernel_initializer=init, name='encoder_%d' % (n_stacks - 1))(h)  
     y = h
-    # internal layers in decoder
+
     for i in range(n_stacks-1, 0, -1):
         y = Dense(dims[i], activation=act, kernel_initializer=init, name='decoder_%d' % i)(y)
 
-    # output
     y = Dense(dims[0], kernel_initializer=init, name='decoder_0')(y)
 
     return Model(inputs=x, outputs=y, name='AE'), Model(inputs=x, outputs=h, name='encoder')
 
 
 class ClusteringLayer(Layer):
-    """
-    Clustering layer converts input sample (feature) to soft label, i.e. a vector that represents the probability of the
-    sample belonging to each cluster. The probability is calculated with student's t-distribution.
 
-    # Example
-    ```
-        model.add(ClusteringLayer(n_clusters=10))
-    ```
-    # Arguments
-        n_clusters: number of clusters.
-        weights: list of Numpy array with shape `(n_clusters, n_features)` witch represents the initial cluster centers.
-        alpha: parameter in Student's t-distribution. Default to 1.0.
-    # Input shape
-        2D tensor with shape: `(n_samples, n_features)`.
-    # Output shape
-        2D tensor with shape: `(n_samples, n_clusters)`.
-    """
 
     def __init__(self, n_clusters, weights=None, alpha=1.0, **kwargs):
         if 'input_shape' not in kwargs and 'input_dim' in kwargs:
@@ -95,13 +61,7 @@ class ClusteringLayer(Layer):
         self.built = True
 
     def call(self, inputs, **kwargs):
-        """ student t-distribution, as same as used in t-SNE algorithm.
-                 q_ij = 1/(1+dist(x_i, u_j)^2), then normalize it.
-        Arguments:
-            inputs: the variable containing data, shape=(n_samples, n_features)
-        Return:
-            q: student's t-distribution, or soft labels for each sample. shape=(n_samples, n_clusters)
-        """
+
         q = 1.0 / (1.0 + (K.sum(K.square(K.expand_dims(inputs, axis=1) - self.clusters), axis=2) / self.alpha))
         q **= (self.alpha + 1.0) / 2.0
         q = K.transpose(K.transpose(q) / K.sum(q, axis=1))
@@ -135,7 +95,6 @@ class FcDEC(object):
         self.datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, rotation_range=10)
         self.autoencoder, self.encoder = autoencoder(self.dims)
 
-        # prepare FcDEC model
         clustering_layer = ClusteringLayer(self.n_clusters, name='clustering')(self.encoder.output)
         self.model = Model(inputs=self.encoder.input, outputs=clustering_layer)
 
@@ -166,7 +125,6 @@ class FcDEC(object):
 
             cb.append(PrintACC(x, y))
 
-        # begin pretraining
         t0 = time()
         if not aug_pretrain:
             self.autoencoder.fit(x, x, batch_size=batch_size, epochs=epochs, callbacks=cb, verbose=verbose)
@@ -194,16 +152,14 @@ class FcDEC(object):
                         yield (batch_x, batch_x)
             self.autoencoder.fit_generator(gen(x, batch_size), steps_per_epoch=int(x.shape[0]/batch_size),
                                            epochs=epochs, callbacks=cb, verbose=verbose)
-            # self.autoencoder.fit_generator(gen(x, batch_size), steps_per_epoch=int(x.shape[0]/batch_size),
-            #                                epochs=epochs, callbacks=cb, verbose=verbose,
-            #                                workers=8, use_multiprocessing=True if platform.system() != "Windows" else False)
+
         print('Pretraining time: ', time() - t0)
         self.autoencoder.save_weights(save_dir + '/ae_weights.h5')
         print('Pretrained weights are saved to %s/ae_weights.h5' % save_dir)
         self.pretrained = True
         print('End pretraining: ', '-' * 60)
 
-    def load_weights(self, weights):  # load weights of FcDEC model
+    def load_weights(self, weights):  
         self.model.load_weights(weights)
 
     def extract_features(self, x):
@@ -213,7 +169,7 @@ class FcDEC(object):
         q = self.model.predict(x, verbose=0)
         return q
 
-    def predict_labels(self, x):  # predict cluster labels using the output of clustering layer
+    def predict_labels(self, x):  
         return np.argmax(self.predict(x), 1)
 
     @staticmethod
@@ -222,14 +178,13 @@ class FcDEC(object):
         return (weight.T / weight.sum(1)).T
 
     def random_transform(self, x):
-        if len(x.shape) > 2:  # image
+        if len(x.shape) > 2:  
             return self.datagen.flow(x, shuffle=False, batch_size=x.shape[0]).next()
 
-        # if input a flattened vector, reshape to image before transform
         width = int(np.sqrt(x.shape[-1]))
-        if width * width == x.shape[-1]:  # gray
+        if width * width == x.shape[-1]: 
             im_shape = [-1, width, width, 1]
-        else:  # RGB
+        else:  
             width = int(np.sqrt(x.shape[-1] / 3.0))
             im_shape = [-1, width, width, 3]
         gen = self.datagen.flow(np.reshape(x, im_shape), shuffle=False, batch_size=x.shape[0])
@@ -248,7 +203,6 @@ class FcDEC(object):
         save_interval = int(maxiter)  # only save the initial and final model
         print('Save interval', save_interval)
 
-        # Step 1: initialize cluster centers using k-means
         t1 = time()
         print('Initializing cluster centers with k-means.')
         kmeans = KMeans(n_clusters=self.n_clusters, n_init=20)
@@ -257,8 +211,6 @@ class FcDEC(object):
         y_pred_last = np.copy(y_pred)
         self.model.get_layer(name='clustering').set_weights([kmeans.cluster_centers_])
 
-        # Step 2: deep clustering
-        # logging file
         import csv, os
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
@@ -286,7 +238,6 @@ class FcDEC(object):
                     logfile.flush()
                     print('Iter %d: acc=%.5f, nmi=%.5f, ari=%.5f; loss=%.5f' % (ite, acc, nmi, ari, avg_loss))
 
-                # check stop criterion
                 delta_label = np.sum(y_pred != y_pred_last).astype(np.float32) / y_pred.shape[0]
                 y_pred_last = np.copy(y_pred)
                 if ite > 0 and delta_label < tol:
@@ -295,7 +246,6 @@ class FcDEC(object):
                     logfile.close()
                     break
 
-            # save intermediate model
             if ite % save_interval == 0:
                 print('saving model to:', save_dir + '/model_' + str(ite) + '.h5')
                 self.model.save_weights(save_dir + '/model_' + str(ite) + '.h5')
